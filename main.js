a42df8f1c43165c2a3f4781f7676f6b98a29cce6
Ryan James Hargrave <66645764+StantyRJ@users.noreply.github.com>
2024-01-20 10:43:12 -0800

    Polished Code Improvements made based on feedback

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // Function to fetch a random European country
 async function getEuropeanCountrys() 
 {
@@ -28,14 +27,11 @@ async function getEuropeanCountrys()
       console.error('Error:', error.message);
     }
   }
-  
-function findCountry(name,europeanCountries)
-{
-  return isEuropeanCountry = europeanCountries.find(country => country.name.common === name);
-}
 
+//function to display pop up messages
 function showMessage(msg,bgCol,anim)
 {
+  $('.correct-message').remove()
   const correctMessage = document.createElement('div');
   correctMessage.className = 'correct-message';
   correctMessage.textContent = msg;
@@ -51,6 +47,7 @@ function showMessage(msg,bgCol,anim)
   }, 1000 * anim - 100);
 }
 
+//function that choses a random question to ask
 function getRandomQuestion(difficultyLevel,categorys)
 {
   availiableCategories = categorys[Math.floor(Math.random() * difficultyLevel)]
@@ -58,6 +55,7 @@ function getRandomQuestion(difficultyLevel,categorys)
   return category;
 }
 
+//gives all the countrys a random color
 function randomColors()
 {
   let colors = ['#855C75','#D9AF6B','#e34a33','#AF6458','#736F4C','#526A83','#625377','#68855C','#9C9C5E','#A06177','#8C785D','#467378','#7C7C7C'];
@@ -70,9 +68,9 @@ function randomColors()
   })
 }
 
+//takes the random category given and displays / formats the question
 function categorysToQuestion(europeanCountries,category)
 {
-
   // Get a random index
   const randomIndex = Math.floor(Math.random() * europeanCountries.length);
 
@@ -84,46 +82,56 @@ function categorysToQuestion(europeanCountries,category)
   switch(category)
   {
     case 'name':
-      $('.HUD').append("<h2>Where is " + randomEuropeanCountry.name.common + "?</h2>")
+      $('.HUD').append('<h2 class="question">Where is ' + randomEuropeanCountry.name.common + '?</h2>')
     break;
 
     case 'flag':
-      $('.HUD').append('<h2>Where is the country with this flag?</h2> <img src="'+randomEuropeanCountry.flags.png+'">')
+      $('.HUD').append('<h2 class="question">Where is the country with this flag?</h2> <img class="question" src="'+randomEuropeanCountry.flags.png+'">')
     break;
 
     case 'neighbors':
-      $('.HUD').append('<h2>What country has these neighbours</h2>')
-      for(i of randomEuropeanCountry.borders)
-        $('.HUD').append(`<p>${i}</p>`)
+      $('.HUD').append('<h2 class="question" >What country has these neighbours</h2>')
 
-      if(randomEuropeanCountry.borders.length == 0)
+      if(randomEuropeanCountry.borders.length > 0)
       {
+        for(i of randomEuropeanCountry.borders)
+          $('.HUD').append(`<p class="question" >${i}</p>`)
+      }
+      else
+      {
+        //two random questions for countries without any neighbours
         if(randomEuropeanCountry.name.common === 'Cyprus')
-          $('.HUD').append('This country has no neighbours (HINT: its half turkish, half greek)')
+          $('.HUD').append('<p class="question">This country has no neighbours (HINT: its half turkish, half greek)</p>')
         else
-          $('.HUD').append('This country has no neighbours (HINT: they bake bread in the ground)')
+          $('.HUD').append('<p class="question">This country has no neighbours (HINT: they bake bread in the ground)</p>')
       }
     break;
 
     case 'capital':
-      $('.HUD').append(`<h2>What country has this capital: ${randomEuropeanCountry.capital[0]}</h2>`)
+      $('.HUD').append(`<h2 class="question">What country has this capital: ${randomEuropeanCountry.capital[0]}</h2>`)
     break
   }
   return randomEuropeanCountry;
 }
 
+//refreshes all the UI on the right of the screen
 function updateUI(lives,score,category,categorys,difficultyLevel,highScore,lastCountry)
 {
   $(".HUD").html(`<p>Lives: ${'X'.repeat(lives)}</p><p>Score: ${score}</p><p>Highscore: ${highScore}</p>`)
   category = getRandomQuestion(difficultyLevel,categorys)
   country = categorysToQuestion(europeanCountries,category)
   while(lastCountry == country.name.common)
+  {
+    $('.question').remove()
     country = categorysToQuestion(europeanCountries,category)
+  }
   return country
 }
 
+//MAIN FUNCTION
 async function setUp()
 {
+  /* DEFAULT VARIABLES */ 
   let lives = 3;
   let score = 0;
   let highScore = 0;
@@ -132,65 +140,95 @@ async function setUp()
   let lost = false;
   categorys = [['name'],['flag'],['neighbors'],['capital']];
   category = ''
+
   $('.retry').hide()
+
   randomColors()
 
+  //generate the list of european countries to use for the rest of the session
   europeanCountries = await getEuropeanCountrys();
-  console.log(europeanCountries)
+  
+  //Update the UI and generate the first question to ask
   let country = updateUI(lives,score,category,categorys,difficultyLevel,highScore,lastCountry)
 
 
+  //handles all interactions with the countries
   $("path").on('click',function(){
-    if(!lost)
+    if(!lost && $('.next').length == 0)
     {
-      countryClicked = findCountry($(this).attr('name'),europeanCountries)
+      countryClicked = europeanCountries.find(country => country.name.common === $(this).attr('name'));
+      /* CORRECT ANSWER */
       if(countryClicked.name.common === country.name.common)
       {
-        
         score += 1000
+
+        //high score logging
         if(score > highScore)
           highScore = score
+
+        //generate new question
         lastCountry = country.name.common
         country = updateUI(lives,score,category,categorys,difficultyLevel,highScore,lastCountry)
         if(score != 5000 && score != 10000 && score != 15000)
-          showMessage('Correct!','green',.5)
+          showMessage('Correct!','green',1)
       }
+      /* INCORRECT ANSWER */
       else
       {
         lives --
-        showMessage('Incorrect!','red',.5)
-        country = updateUI(lives,score,category,categorys,difficultyLevel,highScore,lastCountry)
+        showMessage('Incorrect!','red',1)
+
+        //Show the actual correct answer
+        $('.question').remove()
+        $(`path[name="${country.name.common}"]`).addClass('correct-answer')
+        $('.HUD').append('The correct country was: '+country.name.common)
+
+        //give the next question if you didn't lose
+        if(lives > 0)
+        {
+          $('.UI').append('<button class="next">Next</button>')
+          $('.next').click(function(){
+            $('.next').remove()
+            console.log(country.name.common)
+            $('.correct-answer').removeClass('correct-answer');
+            country = updateUI(lives,score,category,categorys,difficultyLevel,highScore,lastCountry)
+          })
+        }
       }
 
+      //handle loss
       if(lives <= 0)
       {
         lost = true
         $('.retry').show()
       }
 
+      //increses difficulty over time
       if(score >= 5000 && difficultyLevel < 2)
       {
         difficultyLevel++
-        showMessage('Round 2: + Flags','blue',2)
+        showMessage('Round 2: + Flags','blue',3.5)
       }
       else if(score >= 10000 && difficultyLevel < 3)
       {
         difficultyLevel++
-        showMessage('Round 3: + Neighbours','blue',2)
+        showMessage('Round 3: + Neighbours','blue',3.5)
       }
       else if(score >= 15000 && difficultyLevel < 4)
       {
         difficultyLevel++
-        showMessage('Round 3: + Capitals','blue',2)
+        showMessage('Round 3: + Capitals','blue',3.5)
       }
     }
   })
 
+  //resets everything
   $(".retry").on('click',function(){
     lives = 3
     score = 0 
     difficultyLevel = 1
     country = updateUI(lives,score,category,categorys,difficultyLevel,highScore,lastCountry)
+    $('.correct-answer').removeClass('correct-answer');
     lost = false
     $(".retry").hide();
   })
